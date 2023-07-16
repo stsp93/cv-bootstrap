447e876e05bd681204cffdcd701f757fcd3d94dd
stsp93 <stspsp@gmail.com>
2023-07-16 19:54:11 +0300

    name changes and added additional comments

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const scrollTopBtn = document.getElementsByClassName('btn-scrollTop')[0];
 const navBarEl = document.getElementsByClassName('navbar-nav')[1];
 
-const intersectionObserver = new IntersectionObserver(observe);
+// Observe if navbar is intersecting
+const intersectionObserver = new IntersectionObserver(observeCallback);
 
-function observe(entry) {
+intersectionObserver.observe(navBarEl)
+
+function observeCallback(entry) {
+    console.log(entry);
     if (!entry[0].isIntersecting && innerWidth < 992) {
         scrollTopBtn.style.display = "block";
     } else {
@@ -11,8 +15,8 @@ function observe(entry) {
     }
 }
 
-intersectionObserver.observe(navBarEl)
 
+// Scroll to top btn logic
 scrollTopBtn.addEventListener('click', function() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; //For other Browsers
