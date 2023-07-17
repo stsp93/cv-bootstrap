45b780384124f4e1f1499a109d023317af9b0e25
stsp93 <stspsp@gmail.com>
2023-07-17 17:04:21 +0300

    added light theme

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const scrollTopBtn = document.getElementsByClassName('btn-scrollTop')[0];
 const navBarEl = document.getElementsByClassName('navbar-nav')[1];
+const themeBtn = document.getElementById('themeBtn');
 
 // Observe if navbar is intersecting
 const intersectionObserver = new IntersectionObserver(observeCallback);
@@ -7,7 +8,6 @@ const intersectionObserver = new IntersectionObserver(observeCallback);
 intersectionObserver.observe(navBarEl)
 
 function observeCallback(entry) {
-    console.log(entry);
     if (!entry[0].isIntersecting && innerWidth < 992) {
         scrollTopBtn.style.display = "block";
     } else {
@@ -22,3 +22,23 @@ scrollTopBtn.addEventListener('click', function() {
     document.documentElement.scrollTop = 0; //For other Browsers
 })
 
+// Change themes
+
+themeBtn.addEventListener('click', function() {
+
+    document.querySelectorAll('.bg-dark').forEach(e => {
+        e.classList.toggle('bg-dark-light-theme')
+    })
+
+    document.querySelectorAll('.text-light').forEach(e => {
+        e.classList.toggle('text-light-light-theme')
+    })
+
+    document.querySelectorAll('#education span').forEach(e => {
+        e.classList.toggle('education-span-light-theme')
+    })
+
+    document.querySelectorAll('#skills span').forEach(e => {
+        e.classList.toggle('skills-span-light-theme')
+    })
+})
